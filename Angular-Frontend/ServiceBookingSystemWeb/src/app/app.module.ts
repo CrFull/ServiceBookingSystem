0eaef9728c9bc7e7913914574c63658d85402ea5
João Victor <joaocrivoi13@gmail.com>
2024-11-25 17:52:20 -0300

    Making the Company Signup component html, scss and tss to make request for backend(success).

--- Angular-Frontend/ServiceBookingSystemWeb/src/app/app.module.ts
+++ Angular-Frontend/ServiceBookingSystemWeb/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import { HttpClientModule } from '@angular/common/http';
 import { SignupClientComponent } from './basic/components/signup-client/signup-client.component';
+import { SignupCompanyComponent } from './basic/components/signup-company/signup-company.component';
 
 registerLocaleData(en);
 
@@ -22,7 +23,8 @@ registerLocaleData(en);
     AppComponent,
     LoginComponent,
     SignupComponent,
-    SignupClientComponent
+    SignupClientComponent,
+    SignupCompanyComponent
   ],
   imports: [
     BrowserModule,
