48d4c7db4d52f33c8ca74780021a711cb6852522
João Victor <joaocrivoi13@gmail.com>
2024-11-25 17:43:19 -0300

    Configuring the Cors Permission on JavaSpring and making the first request with Client Signup(success).

--- JavaSpring-Backend/ServiceBookingSystem/src/main/java/com/CrFull/ServiceBookingSystem/configs/SimpleCorsFilter.java
+++ JavaSpring-Backend/ServiceBookingSystem/src/main/java/com/CrFull/ServiceBookingSystem/configs/SimpleCorsFilter.java
@@ -1,4 +1,48 @@
-@Order(Ordered.HIGHEST_PREFERENCE)
+package com.CrFull.ServiceBookingSystem.configs;
 
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+
+import java.io.IOException;
+
+@Component
 public class SimpleCorsFilter implements Filter {
+
+    public SimpleCorsFilter()  {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+
+        // Configura os cabeçalhos CORS
+        httpResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:4200"); // Permite apenas o Angular local
+        httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        httpResponse.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, X-Requested-With");
+        httpResponse.setHeader("Access-Control-Allow-Credentials", "true"); // Permite cookies se necessário
+
+
+        if ("OPTIONS".equalsIgnoreCase(httpRequest.getMethod())) {
+            httpResponse.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
