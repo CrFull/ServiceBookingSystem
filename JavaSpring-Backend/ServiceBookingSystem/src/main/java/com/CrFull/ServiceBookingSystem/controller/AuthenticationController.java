f74325ac3d2d7e3c481c5cbeab79a96f4c72b861
João Victor <joaocrivoi13@gmail.com>
2024-11-26 13:49:07 -0300

    Creating the AuthenticationRequest class and the Mapping for the authentication process routes on AuthenticationController.

--- JavaSpring-Backend/ServiceBookingSystem/src/main/java/com/CrFull/ServiceBookingSystem/controller/AuthenticationController.java
+++ JavaSpring-Backend/ServiceBookingSystem/src/main/java/com/CrFull/ServiceBookingSystem/controller/AuthenticationController.java
@@ -1,21 +1,53 @@
 package com.CrFull.ServiceBookingSystem.controller;
 
+import com.CrFull.ServiceBookingSystem.dto.AuthenticationRequest;
 import com.CrFull.ServiceBookingSystem.dto.SignupRequestDTO;
 import com.CrFull.ServiceBookingSystem.dto.UserDto;
+import com.CrFull.ServiceBookingSystem.entity.User;
+import com.CrFull.ServiceBookingSystem.repository.UserRepository;
 import com.CrFull.ServiceBookingSystem.services.authentication.AuthService;
+import com.CrFull.ServiceBookingSystem.services.jwt.UserDetailsServiceImpl;
+import com.CrFull.ServiceBookingSystem.util.JwtUtil;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletResponse;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.webauthn.management.RelyingPartyAuthenticationRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 public class AuthenticationController {
 
     @Autowired
     private AuthService authService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public static final String TOKEN_PREFIX = "Bearer ";
+
+    public static final String HEADER_STRING = "Authorization";
 
     @PostMapping("/client/sign-up")
     public ResponseEntity<?> signupClient(@RequestBody SignupRequestDTO signupRequestDTO){
@@ -34,9 +66,39 @@ public class AuthenticationController {
             return new ResponseEntity<>( "Company already exists with this email!", HttpStatus.NOT_ACCEPTABLE);
         }
 
-        UserDto createUser = authService.signupClient(signupRequestDTO);
+        UserDto createUser = authService.signupCompany(signupRequestDTO);
         return new ResponseEntity<>( createUser, HttpStatus.OK);
 
+    }
+
+
+    @PostMapping({"/authenticate"})
+    public void createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest,
+                                          HttpServletResponse response) throws IOException, JSONException {
+        try{
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    authenticationRequest.getUsername(), authenticationRequest.getPassword()
+            ));
+        }catch (BadCredentialsException e){
+            throw new BadCredentialsException("Incorrect username or password ", e);
+        }
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+        final String jwt = jwtUtil.generateToken(userDetails.getUsername());
+        User user = userRepository.findFirstByEmail(authenticationRequest.getUsername());
+
+        response.getWriter().write(new JSONObject()
+                .put("userId", user.getId())
+                .put("role", user.getRole())
+                .toString()
+        );
+
+        response.addHeader("Access-Control-Expose-Headers", "Authorization");
+        response.addHeader("Access-Control-Allow-Headers", "Authorization," +
+                "X -PINGOTHER, Origin, X-Requested-With, Content-type, Accept, X-Custom-header");
+
+        response.addHeader(HEADER_STRING, TOKEN_PREFIX + jwt);
     }
 
 }
