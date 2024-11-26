c31cb2b6da8b453f55683eb7bf5e563583fab115
João Victor <joaocrivoi13@gmail.com>
2024-11-26 17:57:13 -0300

    Adding the Bean for Password Encoder for Spring Security manage it.

--- JavaSpring-Backend/ServiceBookingSystem/src/main/java/com/CrFull/ServiceBookingSystem/configs/WebSecurityConfig.java
+++ JavaSpring-Backend/ServiceBookingSystem/src/main/java/com/CrFull/ServiceBookingSystem/configs/WebSecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -41,5 +43,10 @@ public class WebSecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
