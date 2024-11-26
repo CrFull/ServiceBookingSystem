f6f8f3187079a6c02208645a5ef0636e33698237
João Victor <joaocrivoi13@gmail.com>
2024-11-26 11:58:59 -0300

    Adding Spring Security, creating Jwt and WebSecurity Confing in Spring boot

--- JavaSpring-Backend/ServiceBookingSystem/src/main/java/com/CrFull/ServiceBookingSystem/util/JwtUtil.java
+++ JavaSpring-Backend/ServiceBookingSystem/src/main/java/com/CrFull/ServiceBookingSystem/util/JwtUtil.java
@@ -62,6 +62,7 @@ public class JwtUtil {
     private Boolean isTokenExpired(String token){
         return extractExpiration(token).before(new Date());
     }
+
     public Boolean validateToken(String token, UserDetails userDetails){
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
